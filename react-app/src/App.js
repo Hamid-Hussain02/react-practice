b53032e3274e194bfa67ed6573f23f30925df58b
Hamid Hussian <hamidhussian@MP15-HamidH-SE-363.local>
2022-03-09 01:01:15 +0500

    counter app

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Counters from "./components/counters";
+import NavBar from "./components/navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 1 },
+      { id: 3, value: 1 },
+      { id: 4, value: 1 },
+    ],
+  };
+  //event handling
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter(
+      (counter) => counter.id !== counterId
+    );
+    this.setState({
+      counters,
+    });
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map((counter) => {
+      counter.value = 0;
+      return counter;
+    });
+    this.setState({
+      counters,
+    });
+  };
+  handleIncrement = (id) => {
+    const counterIndex = this.state.counters.findIndex(
+      (counter) => counter.id === id
+    );
+    const counters = this.state.counters;
+    counters[counterIndex] = {
+      id: id,
+      value: this.state.counters[counterIndex].value + 1,
+    };
+    this.setState({
+      counters: counters,
+    });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar totalCounters={this.state.counters.length} />
+        <Counters
+          counters={this.state.counters}
+          onReset={this.handleReset}
+          onIncrement={this.handleIncrement}
+          onDelete={this.handleDelete}
+        />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
